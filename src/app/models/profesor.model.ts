c588c8bc0a7cb807dd1f596ce51c98ad94b08c3e
GINOXCVIII <nunezolmos.ig@gmail.com>
2022-10-07 18:08:05 -0300

    agregado modelos y esas cosas

---
+++ src/app/models/profesor.model.ts
@@ -0,0 +1,9 @@
+export class Profesor {
+	id?: number;
+	profesion?: String;
+	id_categoria?: number;
+	nombre_categoria?: String;
+	importe_por_hora?: number;
+	id_docente?: number;
+	nombre_docente?: String;
+}
---
+++ src/app/models/socio.model.ts
@@ -0,0 +1,7 @@
+export class Socio {
+	id?: number;
+	antiguedad?: number;
+	valor_acordado?: number;
+	id_docente?: number;
+	nombre?: String;
+}
